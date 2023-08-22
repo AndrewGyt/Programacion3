3a60278a7c4b4c52860d6bdc8ddc29c2a01c534a
Andre <andre_rf97@hotmail.com>
2023-08-22 16:50:39 -0400

    2023-22-08_01

---
+++ ahorcado.py
@@ -0,0 +1,6 @@
+def jugar_ahorcado():
+    print("=================================================")
+    print("BIENVENIDO AL JUEGO DE LA HORCA")
+    print("=================================================")
+
+    print("Fin del Juego")
