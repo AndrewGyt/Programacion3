9e4e77a9d72a491a14ecdffdbd3bcdbcb8e08118
Andre <andre_rf97@hotmail.com>
2023-08-29 18:13:46 -0400

    version 2023-08-29_05

--- ahorcado.py
+++ ahorcado.py
@@ -3,13 +3,14 @@ def jugar():
     print('================================')
     print('Bienvenido al Juego del Ahorcado')
     print('================================')
-    
-     
+        
     palabra_secreta = 'mandarina'
+    letras_acertadas=["_","_","_","_","_","_","_","_","_"]
     
     ahorcado = False
     acerto = False
     
+    print(letras_acertadas)
     while ( not ahorcado and not acerto ):
         entrada=input ("ingrese una letra...")
         entrada=entrada.strip()
@@ -17,11 +18,11 @@ def jugar():
         indice = 0
         for letra in palabra_secreta:
             if(entrada==letra):
-                print("Se encontro la letra {} en la posicion {}".format(letra,indice))
+                letras_acertadas[indice] = letra
                 
-            indice = indice + 1    
-                
-        print('jugando...')
+            indice = indice + 1  
+           
+        print(letras_acertadas)       
 
     print("Fin del Juego")
     
